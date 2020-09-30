b0663405b3a93704c4952f142a870a49375739c8
Dylan Tsuruda <tsuruda.dylan@yahoo.com>
2020-09-29 17:15:13 -1000

    Modify settings.py again

--- BassTromboneSpecs/settings.py
+++ BassTromboneSpecs/settings.py
@@ -131,23 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 django_heroku.settings(locals())
