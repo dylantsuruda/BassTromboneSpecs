5e36f3745992be8541e9aece422a2998d27f0adf
Dylan Tsuruda <tsuruda.dylan@yahoo.com>
2020-09-29 14:36:28 -1000

    Fix small errors for deployment

--- BassTromboneSpecs/settings.py
+++ BassTromboneSpecs/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ['DJANGO_DEBUG'] == 'True' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://basstrombonespecs.herokuapp.com/',
+]
 
 
 # Application definition
@@ -40,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'specs.apps.SpecsConfig',
-    'feedback.apps.FeedbackConfig'
+    'feedback.apps.FeedbackConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,8 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
