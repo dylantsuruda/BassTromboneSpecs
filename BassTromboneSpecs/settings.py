bbc2c491ba4cd0e0b3f53347d21c22ea553fd9be
Dylan Tsuruda <tsuruda.dylan@yahoo.com>
2020-09-29 14:57:01 -1000

    Fix more database errors for deployment

--- BassTromboneSpecs/settings.py
+++ BassTromboneSpecs/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,5 +132,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals())
