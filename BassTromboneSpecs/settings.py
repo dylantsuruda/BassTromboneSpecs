614d001f905e46a78fb6e224ce335b11c969ec6b
Dylan Tsuruda <tsuruda.dylan@yahoo.com>
2020-09-29 15:20:20 -1000

    Fix database deployment errors

--- BassTromboneSpecs/settings.py
+++ BassTromboneSpecs/settings.py
@@ -132,5 +132,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals())
